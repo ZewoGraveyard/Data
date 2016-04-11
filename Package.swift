93ddfcfa7377093b6b661f3d7601377b24271a92
Scott Byrns <contact@scottbyrns.com>
2016-04-10 22:56:11 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -28,6 +28,6 @@ let package = Package(
     name: "Data",
     dependencies: [
         .Package(url: "https://github.com/Zewo/OS.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 4),
     ]
 )
