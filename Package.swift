aeb92644d7ceb38810475b3b626f23cd27c13a65
David Ask <david@formbound.com>
2016-04-20 07:16:15 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ import PackageDescription
 let package = Package(
     name: "Data",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/OS.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/POSIX.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 4),
     ]
 )
